606589633d7c90f484561ed571c2138670fca86d
Lena Faure <lfaure@lfaure-ltm2.internal.salesforce.com>
2019-03-17 14:21:39 -0700

    Declare variables

--- cartridges/cultura_front/cartridge/client/default/js/components/cultura_front.js
+++ cartridges/cultura_front/cartridge/client/default/js/components/cultura_front.js
@@ -5,30 +5,35 @@
  */
 
 $(document).ready(function () {
+    var productsBtn = $(".top-menu");
+    var megaMenuWrapper = $(".menu-group");
+    var rootCategoriesWrapper = $(".navbar-nav");
+    var rootCategory = $(".nav-item");
+    var subCategoriesWrapper = $(".sub-menu-wrapper");
+    var categoryTitle = $(".sub-menu-category-title");
 
     // Show root categories menu on hover
-    if($(".top-menu").length){
-        $(".top-menu").hover(function () {
-            $(".navbar-nav").fadeIn();
+    if(productsBtn.length){
+        productsBtn.hover(function () {
+            rootCategoriesWrapper.fadeIn();
         });
     }
 
     // Show subcategories on root category nav item hover
-    if($(".nav-item").length){
-        $(".nav-item").hover(function () {
-            $(".sub-menu-wrapper").show();
+    if(rootCategory.length){
+        rootCategory.hover(function () {
+            subCategoriesWrapper.show();
 
             // Display category name as title on right panel
-            var categoryTitle = $(".sub-menu-category-title");
             categoryTitle.html($(this).attr("data-category"));
         });
     }
 
     // Hide mega menu on mouseleave
-    if($(".menu-group").length){
-        $(".menu-group").mouseleave(function () {
-            $(".navbar-nav").hide();
-            $(".sub-menu-wrapper").hide();
+    if(megaMenuWtapper.length){
+        megaMenuWrapper.mouseleave(function () {
+            rootCategoriesWrapper.hide();
+            subCategoriesWrapper.hide();
         });
     }
 
